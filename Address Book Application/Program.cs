b94e19c0c1a684abb242cd71c5c3d48e10c12348
ObsidianAsphodel <terencep.dixon@gmail.com>
2025-08-04 11:00:37 -0700

    Started Address Book Application Project

---
+++ Address Book Application/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AddressBook;
+
+class Program
+{
+
+    List<Contact> AddresBook = new List<Contact>();
+    public string ownerName = "";
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Good Evening! Here are your current contacts");
+        ViewContacts();
+    }
+
+    public static void ViewContacts()
+    {
+
+    }
+    public static void AddContacts()
+    {
+
+    }
+    public static void EditContacts()
+    {
+
+    }
+    public static void DeleteContacts()
+    {
+
+    }
+    public static void Exit()
+    {
+        Console.Clear();
+    }
+}
+class Contact
+{
+    public static string Name = "";
+    public static string Address = ""; 
+}
+// Show List Of Address Book
